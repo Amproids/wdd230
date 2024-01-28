faf1edb1360d323c3aa7831ba9ce2bb05a084584
Andrew Parry <parryandrew47@gmail.com>
2024-01-27 23:15:20 -0700

    added qualities for w03 assignment

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,11 @@
-let lasModifiedElement = document.getElementById("lastModified");
-let copyWriteYearElement = document.getElementById("copyright");
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
+const lasModifiedElement = document.getElementById("lastModified");
+const copyWriteYearElement = document.getElementById("copyright");
 
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
 copyWriteYearElement.textContent = `©${new Date().getFullYear()} | Andrew Parry | Utah, USA`;
 lasModifiedElement.textContent = `Last Modification: ${document.lastModified}`;
