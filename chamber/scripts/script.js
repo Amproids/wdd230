0e0d2a6c271482c4de69c727c2897e3b21ff255c
Andrew Parry <parryandrew47@gmail.com>
2024-02-13 23:24:33 -0700

    added normalize css

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,6 +1,5 @@
-
-const lastModifiedElement = document.getElementById("modification-date");
 const copyrightYearElement = document.getElementById("current-year");
+const lastModifiedElement = document.getElementById("modification-date");
 
 copyrightYearElement.textContent = `${new Date().getFullYear()}`;
 lastModifiedElement.textContent = `${document.lastModified}`;
