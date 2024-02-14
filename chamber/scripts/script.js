fd7eda6e477f09d9522b67b2063bb281f1fd4a17
Andrew Parry <parryandrew47@gmail.com>
2024-02-13 23:50:37 -0700

    added hamburger menu functionality

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,5 +1,12 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
 const copyrightYearElement = document.getElementById("current-year");
 const lastModifiedElement = document.getElementById("modification-date");
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
 
 copyrightYearElement.textContent = `${new Date().getFullYear()}`;
 lastModifiedElement.textContent = `${document.lastModified}`;
