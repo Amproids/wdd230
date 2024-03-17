8269f8cd4cfef23ad6606bc60b9325e86b6e8aff
Andrew Parry <parryandrew47@gmail.com>
2024-03-17 07:05:22 -0600

    added dynamic assignments to home page (I don't remember how I made it work)

--- scripts/links.js
+++ scripts/links.js
@@ -1,8 +1,24 @@
-const baseURL = "https://Amproids.bithub.io/wdd230/";
-const linksURL = "https://Amproids.bithub.io/wdd230/data/links.json";
+const baseURL = "https://Amproids.github.io/wdd230/";
+const linksURL = "https://Amproids.github.io/wdd230/data/links.json";
+const linksElement = document.querySelector("#lessons");
+
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data.lessons);
 }
+
+const displayLinks = (lessons) => {
+    lessons.forEach(lesson => {
+        let item = document.createElement("li");
+        item.innerHTML = `${lesson.lesson}: `;
+        lesson.links.forEach(link => {
+            item.innerHTML += `<a href="${link.url}">${link.title}</a> | `;
+        })
+        item.innerHTML = item.innerHTML.slice(0, -3);
+        linksElement.appendChild(item);
+    });
+}
+
+getLinks();
