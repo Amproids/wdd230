00a0d1872672cecd8bb4ad60ca5cfa2b1f7c5c32
Andrew Parry <parryandrew47@gmail.com>
2024-03-24 21:41:08 -0600

    added weather API to lesson 10

---
+++ lesson10/scripts/weather.js
@@ -0,0 +1,30 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.7565&lon=6.6412&units=imperial&appid=d665ebd1da3ac4f6cce02014053f4591';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.textContent = `${data.main.temp} °F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
+apiFetch();
