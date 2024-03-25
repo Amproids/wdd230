08f40da93d0ba46dc3765a02e5d591be648ad1aa
Andrew Parry <parryandrew47@gmail.com>
2024-03-24 22:52:04 -0600

    finished W10 Weather home page assignment.

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,14 +1,13 @@
-// select HTML elements in the document
-const currentTemp = document.querySelector('#current-temp');
+const weatherElement = document.querySelector('#weather');
 const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.7565&lon=6.6412&units=imperial&appid=d665ebd1da3ac4f6cce02014053f4591';
 
-async function apiFetch() {
+async function getWeatherData() {
     try {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
+            console.log(data);
             displayResults(data);
         } else {
             throw Error(await response.text());
@@ -19,11 +18,11 @@ async function apiFetch() {
 }
 
 function displayResults(data) {
-    currentTemp.textContent = `${data.main.temp} °F`;
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     let desc = data.weather[0].description;
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = `${desc}`;
+    weatherElement.textContent = ` ${data.main.temp} °F - ${desc}`;
 }
-apiFetch();
+
+getWeatherData();
