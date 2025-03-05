f8fa25f84983fa5b2d710b228358aec8bbe223a8
Andrew Parry <parryandrew47@gmail.com>
2025-03-04 22:18:22 -0700

    finished W01 home page assignment

---
+++ scripts/getDates.js
@@ -0,0 +1,5 @@
+// Get the current year for the copyright
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// Get the last modified date
+document.getElementById('lastModified').textContent = document.lastModified;
