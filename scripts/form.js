0f558fb2f75ea91f97cb6597302a3bcf6c4eaf87
Andrew Parry <parryandrew47@gmail.com>
2025-04-01 15:48:27 -0600

    fix: fixed email validation logic

--- scripts/form.js
+++ scripts/form.js
@@ -1,7 +1,7 @@
-// Get DOM elements
 const form = document.querySelector('form');
 const password1 = document.getElementById('password');
 const password2 = document.getElementById('password2');
+const email = document.getElementById('email');
 const ratingInput = document.getElementById('rating');
 const ratingValue = document.querySelector('.rating-value');
 
@@ -14,13 +14,20 @@ ratingInput.addEventListener('input', function() {
 form.addEventListener('submit', function(event) {
     // Check if passwords match
     if (password1.value !== password2.value) {
-        event.preventDefault(); // Prevent form submission
+        event.preventDefault();
         alert('Passwords do not match. Please try again.');
-        // Clear password fields
         password1.value = '';
         password2.value = '';
-        // Focus back to the first password field
         password1.focus();
+    }
+
+    // Check if email is valid
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@byui\.edu$/;
+    if (!emailPattern.test(emailPattern)) {
+        event.preventDefault();
+        alert('Please enter a valid email address from byui.edu domain.');
+        email.value = '';
+        email.focus();
     }
 });
 
