f546bd2ed0695444d40cc92be2c488ddb9ee9f8f
Andrew Parry <parryandrew47@gmail.com>
2025-04-03 23:16:00 -0600

    feat: adding js and json for adding in links

--- scripts/links.js
+++ scripts/links.js
@@ -4,8 +4,27 @@ const linksURL = "https://amproids.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
+    displayLinks(data);
+}
+
+function displayLinks(data) {
+    const links = document.querySelector('ul');
+    data.weeks.forEach(week => {
+        const li = document.createElement('li');
+        const h2 = document.createElement('h3');
+        h2.textContent = week.week;
+        li.appendChild(h2);
+        week.links.forEach(link => {
+            const a = document.createElement('a');
+            a.setAttribute('href', baseURL + link.url);
+            a.textContent = link.title;
+            li.appendChild(a);
+            const br = document.createElement('br');
+            li.appendChild(br);
+        });
+        links.appendChild(li);
+    })
     console.log(data);
-    return data;
 }
 
 getLinks();
